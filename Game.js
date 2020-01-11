1f0556d61e682ec276bd4fb506066dcdce572f03
VINAY GADWAL <vinay.gadwal@kuliza.com>
2020-01-11 17:12:43 +0530

    tic tac game

--- Game.js
+++ Game.js
@@ -1,86 +1,173 @@
-import React, {useState, useEffect} from 'react';
-import {Text, StyleSheet, View} from 'react-native';
+import React, {Component} from 'react'
+import {StyleSheet, View, TouchableOpacity,Text} from 'react-native'
 
-const GRID_LENGTH = 3;
-let turn = 'X';
-
-const Game = () => {
-  const [grid, setGrid] = useState([]);
-
-  const initializeGrid = () => {
-    const tempGrid = [];
-    for (let colIdx = 0; colIdx < GRID_LENGTH; colIdx++) {
-      const tempArray = [];
-      for (let rowidx = 0; rowidx < GRID_LENGTH; rowidx++) {
-        tempArray.push(0);
-      }
-      tempGrid.push(tempArray);
+export default class Game extends Component{
+  constructor(){
+    super(),
+    this.state = {
+      initState: [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+      ],
+      currentPlayer: 1
     }
-    setGrid(tempGrid);
-  };
-
-  const getBox = (index, colIdx, rowIdx) => {
-    let backgroundColor = 'red';
-    const sum = colIdx + rowIdx;
-    if (sum % 2 === 0) {
-      backgroundColor = 'blue';
-    }
-    const gridValue = grid[colIdx][rowIdx];
-    let content = '-';
-    if (gridValue === 1) {
-      content = 'X';
-    } else if (gridValue === 2) {
-      content = 'O';
-    }
-    return (
-      <View key={index} styles={{...styles.boxStyle, backgroundColor}}>
-        <Text>{content}</Text>
-      </View>
-    );
-  };
-
-  const getRow = (row, colIdx) => {
-    return row.map((item, index) => {
-      return getBox(index, colIdx, index);
-    });
-  };
-
-  const getColumns = () => {
-    return grid.map((row, index) => {
-      return (
-        <View style={styles.rowStyle} key={index}>
-          {getRow(row, index)}
-        </View>
-      );
-    });
-  };
-
-  const renderMainGrid = () => {
-    return (
-      <View style={{display: 'flex'}}>
-        <View style={styles.columnsStyle}>{getColumns()}</View>
-      </View>
-    );
-  };
-
-  useEffect(() => {
-    initializeGrid();
-  }, []);
-
-  if (grid.length === 0) {
-    return <Text> initializing </Text>;
   }
-  return renderMainGrid();
-};
 
+  componentDidMount(){
+    this.initGame();
+  }
+  
+  initGame = () => {
+    this.setState({
+      initState: [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+      ]
+    })
+  }
+
+  finalWinner = () => {
+    const num_row_col = 3;
+    var arr = this.state.initState;
+    var sum;
+
+    for (var i = 0; i < num_row_col; i++){
+      sum = arr[i][0]+arr[i][1]+arr[i][2];
+      if(sum == 3)
+        return 1;
+      else if(sum == -3)
+        return -1;
+    }
+
+    for (var i = 0; i < num_row_col; i++){
+      sum = arr[0][i]+arr[1][i]+arr[2][i];
+        if(sum == 3)
+          return 1;
+        else if(sum == -3)
+          return -1;
+    }
+
+    sum = arr[0][0] + arr[1][1] + arr[2][2];
+      if(sum == 3)
+        return 1;
+      else if(sum == -3)
+        return -1;
+
+    sum = arr[2][0] + arr[1][1] + arr[0][2];
+      if(sum == 3)
+        return 1;
+      else if(sum == -3)
+        return -1;
+
+    return 0;
+  }
+  gameHandler = (r,c) => {
+    console.log(r,c)
+    var value = this.state.initState[r][c];
+    switch(value)
+    {
+      case 1: return <Text style={styles.tileStyleX}>X</Text>;
+      case -1: return <Text style={styles.tileStyleO}>O</Text>
+      default: return <View/>
+    }
+  }
+
+  onPressTiles = (r,c) => {
+      var ifSelected = this.state.initState[r][c];
+      if(ifSelected !== 0)
+        return ;
+
+      var currentPlayer = this.state.currentPlayer;
+      var arr = this.state.initState.slice();
+      arr[r][c]= currentPlayer;
+      this.setState({ initState: arr})
+
+      var nextPlayer = ( currentPlayer === 1) ? -1 : 1;
+      this.setState({currentPlayer: nextPlayer})
+
+      var winner = this.finalWinner()
+      if(winner == 1){
+        alert("player 1 winner")
+        this.initGame();
+      }
+      else if(winner == -1){
+        alert("player 2 winner")
+        this.initGame();
+      }
+  }
+
+  render(){
+    return(
+      <View style={styles.constainer}>
+        <View style={styles.directionRow}> 
+          <TouchableOpacity onPress={() =>this.onPressTiles(0,0)} style={styles.box}>
+           {this.gameHandler(0,0)}
+          </TouchableOpacity>
+          
+          <TouchableOpacity onPress={() =>this.onPressTiles(0,1)} style={styles.box}>
+            {this.gameHandler(0,1)}
+          </TouchableOpacity>
+          
+          <TouchableOpacity onPress={() =>this.onPressTiles(0,2)} style={styles.box}>
+            {this.gameHandler(0,2)}
+          </TouchableOpacity>
+        </View>
+        <View style={styles.directionRow}>
+          <TouchableOpacity onPress={() =>this.onPressTiles(1,0)} style={styles.box}>
+            {this.gameHandler(1,0)}
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() =>this.onPressTiles(1,1)} style={styles.box}>
+            {this.gameHandler(1,1)}
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() =>this.onPressTiles(1,2)} style={styles.box}>
+            {this.gameHandler(1,2)}
+          </TouchableOpacity>
+        </View>
+        <View style={styles.directionRow}>
+          <TouchableOpacity onPress={() =>this.onPressTiles(2,0)} style={styles.box}>
+            {this.gameHandler(2,0)}
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() =>this.onPressTiles(2,1)} style={styles.box}>
+            {this.gameHandler(2,1)}
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() =>this.onPressTiles(2,2)} style={styles.box}>
+            {this.gameHandler(2,2)}
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
+
+  }
+}
 const styles = StyleSheet.create({
-  columnsStyle: {
-    flexDirection: 'column',
+  constainer: {
+    alignContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 100
   },
-  rowStyle: {
-    flexDirection: 'row',
+  box: {
+    borderWidth: 1,
+    width: 100,
+    height: 100
   },
-  boxStyle: {width: 100, height: 100},
-});
-
-export default Game;
+  directionRow: {
+    flexDirection: 'row'
+  },
+  tileStyleX: {
+    fontSize: 70,
+    fontWeight: 'bold',
+    color: 'red',
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+  tileStyleO: {
+    fontSize: 70,
+    fontWeight: 'bold',
+    color: 'blue',
+    alignContent: 'center',
+    justifyContent: 'center',
+  }
+})
